97149c44932a36da6b42644bc268994e183decc5
rabobahago <rabobahago@gmail.com>
2024-07-02 17:03:48 +0100

    linear search algorithms implemented

--- data_structure/list/main.py
+++
@@ -1,7 +0,0 @@
-def last_item(inventory):
-
-    # account for potential edge cases that could cause the function to fail!
-    if not inventory:
-        return None
-    return inventory[-1]
-print(not False)
