891340a15f55d5e63b812616e121091d675f5f4d
rabobahago <rabobahago@gmail.com>
2025-01-16 12:13:18 +0100

    player letter choose function added

--- tictactoe_game/tictactoe.py
+++ tictactoe_game/tictactoe.py
@@ -1,7 +1,18 @@
+import random
 def drawBoard(board):
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
-drawBoard([' ', 'O', ' ', 'X', ' ', 'O', ' ', ' ', ' ', 'X'])
+# drawBoard([' ', 'O', ' ', 'X', ' ', 'O', ' ', ' ', ' ', 'X'])
+
+def inputPlayerLetter():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        letter = input('Do you want to be X or O? ').upper()
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+print(inputPlayerLetter())
